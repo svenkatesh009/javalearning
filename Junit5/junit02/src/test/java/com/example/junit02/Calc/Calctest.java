0f47a16e511d8aa5c7c68406e8471fe1f6513436
Samudi <venkatesh.samudi@accenture.com>
2025-01-08 05:54:31 +0530

    chenges done

--- Junit5/junit02/src/test/java/com/example/junit02/Calc/Calctest.java
+++ Junit5/junit02/src/test/java/com/example/junit02/Calc/Calctest.java
@@ -7,15 +7,15 @@ public class Calctest {
     @Test
     public void test(){
      Calc c=new Calc();
-     int actual=c.divide(10, 2);
-     int expected=5;
+     int actual=c.divide(20, 2);
+     int expected=10;
      assertEquals(expected, actual);
 }
 @Test
 public void test2(){
  Calc c=new Calc();
- int actual=c.divide(30, 2);
- int expected=15;
+ int actual=c.divide(30, 5);
+ int expected=6;
  assertEquals(expected, actual);
 }
 }
