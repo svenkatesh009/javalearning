797f3e3d5f268d23ec9214ca20630e1035503a26
svenkatesh009 <117891450+svenkatesh009@users.noreply.github.com>
2025-03-14 17:00:01 +0530

    Add files via upload

---
+++ Servelet&JSP/DemoApp/src/main/java/com/practice/AddServelet.java
@@ -0,0 +1,19 @@
+package com.practice;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddServelet extends HttpServlet {
+
+	public void service(HttpServletRequest req,HttpServletResponse res) throws IOException{
+		int num1=Integer.parseInt(req.getParameter("num1"));
+		int num2=Integer.parseInt(req.getParameter("num2"));
+		int result=num1+num2;
+		PrintWriter out=res.getWriter();
+		out.println("result is"+result);
+	}
+}
